2b1629fbf56c68eee11e768dfec53ab0e3214e43
joseph <josephachonu@gmail.com>
2021-01-05 23:34:29 +0100

    added nav bar animation

--- js/index.js
+++ js/index.js
@@ -1,4 +1,16 @@
-//function to toggle nav bar
+//function to check if element is in view port
+var isInViewport = function (elem) {
+    var bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+
+
+//Event listener to show nav bar when button is clicked
 document.getElementById("hide-nav").addEventListener("click",()=>{
     document.getElementById("hide-nav").style.display="none";
     document.getElementById("show-nav").style.display="block";
@@ -6,6 +18,7 @@ document.getElementById("hide-nav").addEventListener("click",()=>{
 
 })
 
+//Event listener to hide nav bar when button is clicked
 document.getElementById("show-nav").addEventListener("click",()=>{
     document.getElementById("hide-nav").style.display="block";
     document.getElementById("show-nav").style.display="none";
@@ -13,8 +26,7 @@ document.getElementById("show-nav").addEventListener("click",()=>{
 
 })
 
-
-
+//Event listener to display scroll back to top icon
 document.addEventListener("scroll",function(){
     var element=document.getElementById("top")
     if(isInViewport(element)){
@@ -29,14 +41,7 @@ window.onload = function() {
    }
 
 
-var isInViewport = function (elem) {
-    var bounding = elem.getBoundingClientRect();
-    return (
-        bounding.top >= 0 &&
-        bounding.left >= 0 &&
-        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-};
 
-
+//animation using gsap
+gsap.from("#top",{duration:1,ease:"bounce",y:"-100%"})
+gsap.from(".nav-link",{duration:1,delay:1,opacity:0,stagger:.2})
